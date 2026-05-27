075af509e32295d492a096e132ebf84d3e06c792
minihauner <jannickhaunstrup@gmail.com>
2026-05-27 21:39:24 +0200

    change from commonjs to es modules

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
