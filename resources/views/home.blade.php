c435436da0a29acdbfe0512483a4789d442a6c11
minihauner <jannickhaunstrup@gmail.com>
2026-07-13 17:13:28 +0200

    do some very minor markup and set up a few base styles

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+    <header>
+        <a href="#">pertineo</a>
+        <nav>
+            <ul>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="section hero">
+                <h1 class="hero__heading">pertineo</h1>
+                <p class="hero__sub-heading">Lorem ipsum dolor sit amet,<br>curabitur blandit tempus porttitor.</p>
+
+                <a href="#" class="button">Try it out now!</a>
+        </section>
+    </main>
+
+    <footer></footer>
+</body>
+</html>
