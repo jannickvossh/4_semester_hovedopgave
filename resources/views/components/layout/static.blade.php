b3c1f99db5e754f2b670ca009d49ab1857e72c7f
minihauner <jannickhaunstrup@gmail.com>
2026-07-23 13:03:28 +0200

    add fonts from typekit

--- resources/views/components/layout/static.blade.php
+++ resources/views/components/layout/static.blade.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="https://use.typekit.net/doy8nke.css">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
