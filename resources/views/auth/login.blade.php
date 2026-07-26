dc1b8562ebe078073c4f081aa04178fd49856714
minihauner <jannickhaunstrup@gmail.com>
2026-07-26 22:04:49 +0200

    custom classes for margin top and bottom + finishing touches for now on register and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,47 @@
 <x-layout.static title="Log in | Pertineo">
     <section class="section">
         <div class="section__inner">
-            <h1>Log in to your account</h1>
+            <h1 class="text-align-center">Log in to your account</h1>
 
-            <form method="POST" action="{{ route('sessions.store') }}">
-                @csrf
+            <div class="container--very-narrow mx-auto mt-large">
+                <form method="POST" action="{{ route('sessions.store') }}" class="form mb-large">
+                    @csrf
 
-                <div class="form__field">
-                    <label class="label" for="email">E-mail</label>
-                    <input
-                        class="input input--email"
-                        type="email"
-                        name="email"
-                        id="email"
-                        autofocus
-                    >
+                    <div class="form__field">
+                        <label class="label" for="email">E-mail</label>
+                        <input
+                            class="input input--email"
+                            type="email"
+                            name="email"
+                            id="email"
+                            autofocus
+                        >
 
-                    @error('email')
-                    <p class="error">{{ $message }}</p>
-                    @enderror
-                </div>
+                        @error('email')
+                        <p class="error">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                <div class="form__field">
-                    <label class="label" for="password">Password</label>
-                    <input
-                        class="input input--password"
-                        type="password"
-                        name="password"
-                        id="password"
-                    >
+                    <div class="form__field">
+                        <label class="label" for="password">Password</label>
+                        <input
+                            class="input input--password"
+                            type="password"
+                            name="password"
+                            id="password"
+                        >
 
-                    @error('password')
-                    <p class="error">{{ $message }}</p>
-                    @enderror
-                </div>
+                        @error('password')
+                        <p class="error">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                <button type="submit" class="button button--solid">Log in</button>
-            </form>
+                    <button type="submit" class="button button--solid mx-auto">Log in</button>
+                </form>
 
-            <p>Not registered yet?</p>
-            <a href="{{ route('register') }}" class="button button--outline">Register here</a>
+                <p class="text-align-center mb-tiny">Don't have an account yet?</p>
+                <a href="{{ route('register') }}" class="button button--outline mx-auto">Register here</a>
+            </div>
         </div>
     </section>
 </x-layout.static>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,84 @@
 <x-layout.static title="Register | Pertineo">
     <section class="section">
         <div class="section__inner">
-            <h1>Sign up for a new account</h1>
+            <h1 class="text-align-center">Sign up for a new account</h1>
 
-            <p><span class="color-lightblue">*</span> Required fields</p>
+            <div class="container--narrow mx-auto mt-large">
+                <form method="POST" action="{{ route('register.store') }}" class="form mb-large">
+                    @csrf
 
-            <form method="POST" action="{{ route('register.store') }}" class="form">
-                @csrf
+                    <p><span class="color-lightblue">*</span> Required fields</p>
 
-                <div class="form__group">
-                    <div class="form__field">
-                        <label class="label has-asterisk" for="firstName">First name</label>
-                        <input
-                            class="input input--text"
-                            type="text"
-                            name="firstName"
-                            id="firstName"
-                            placeholder="Jane"
-                            autofocus
-                        >
+                    <div class="form__group">
+                        <div class="form__field">
+                            <label class="label has-asterisk" for="firstName">First name</label>
+                            <input
+                                class="input input--text"
+                                type="text"
+                                name="firstName"
+                                id="firstName"
+                                placeholder="Jane"
+                                autofocus
+                            >
 
-                        @error('firstName')
-                        <p class="error">{{ $message }}</p>
-                        @enderror
+                            @error('firstName')
+                            <p class="error">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="form__field">
+                            <label class="label has-asterisk" for="lastName">Last name</label>
+                            <input
+                                class="input input--text"
+                                type="text"
+                                name="lastName"
+                                id="lastName"
+                                placeholder="Doe"
+                            >
+
+                            @error('lastName')
+                            <p class="error">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
 
-                    <div class="form__field">
-                        <label class="label has-asterisk" for="lastName">Last name</label>
-                        <input
-                            class="input input--text"
-                            type="text"
-                            name="lastName"
-                            id="lastName"
-                            placeholder="Doe"
-                        >
+                    <div class="form__group">
+                        <div class="form__field">
+                            <label class="label has-asterisk" for="email">E-mail</label>
+                            <input
+                                class="input input--email"
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="example@domain.com"
+                            >
 
-                        @error('lastName')
-                        <p class="error">{{ $message }}</p>
-                        @enderror
+                            @error('email')
+                            <p class="error">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="form__field">
+                            <label class="label has-asterisk" for="password">Password</label>
+                            <input
+                                class="input input--password"
+                                type="password"
+                                name="password"
+                                id="password"
+                            >
+
+                            @error('password')
+                            <p class="error">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
-                </div>
 
-                <div class="form__group">
-                    <div class="form__field">
-                        <label class="label has-asterisk" for="email">E-mail</label>
-                        <input
-                            class="input input--email"
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="example@domain.com"
-                        >
+                    <button class="button button--solid mx-auto" type="submit">Register</button>
+                </form>
 
-                        @error('email')
-                        <p class="error">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="form__field">
-                        <label class="label has-asterisk" for="password">Password</label>
-                        <input
-                            class="input input--password"
-                            type="password"
-                            name="password"
-                            id="password"
-                        >
-
-                        @error('password')
-                        <p class="error">{{ $message }}</p>
-                        @enderror
-                    </div>
-                </div>
-
-                <button class="button button--solid" type="submit">Register</button>
-            </form>
-
-            <p>Already have an account?</p>
-            <a href="{{ route('login') }}" class="button button--outline">Log in here</a>
+                <p class="text-align-center mb-tiny">Already have an account?</p>
+                <a href="{{ route('login') }}" class="button button--outline mx-auto">Log in here</a>
+            </div>
         </div>
     </section>
 </x-layout.static>
