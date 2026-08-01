02e8e4c1961a6bfa617aeecb961e4248a4c320f7
minihauner <jannickhaunstrup@gmail.com>
2026-08-01 21:45:49 +0200

    layout and styling for vertical header

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,8 @@
-<x-layout.dashboard title="Home | Pertineo"></x-layout>
+<x-layout.dashboard title="Home | Pertineo">
+    <form method="POST" action="{{ route('sessions.destroy') }}">
+        @csrf
+        @method('DELETE')
+
+        <button class="button" type="submit">Log out</button>
+    </form>
+</x-layout.dashboard>
